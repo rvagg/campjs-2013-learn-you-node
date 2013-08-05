0fd5afc8768697de2329faf0f6be02e17821c029
Rod Vagg <rod@vagg.org>
2013-08-05 14:32:04 +1000

    added example server

---
+++ presentation/example_server.js
@@ -0,0 +1,25 @@
+var net     = require('net')
+var sockets = {}
+
+var server = net.createServer(function (socket) {
+  var id = socket.remoteAddress + ':' + socket.remotePort
+  sockets[id] = socket
+
+  socket.on('end', function () {
+    delete sockets[id]
+  })
+
+  socket.on('data', function (data) {
+    msg('<' + id + '> ' + data)
+  })
+
+  msg('* ' + id + ' joined the chat')
+})
+
+function msg (msg) {
+  Object.keys(sockets).forEach(function (peer) {
+    sockets[peer].write(msg.replace(/(\r\n)+/g, '') + '\n')
+  })
+}
+
+server.listen(1337)
